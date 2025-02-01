85025cf7c166bca64079b6416ca9407ac17cf015
ericwooshem <97374556+ericwooshem@users.noreply.github.com>
2025-02-01 14:31:12 -0800

    Field Recorder Done (I hope)

--- DashboardCore/src/test/java/com/acmerobotics/dashboard/OrbitOverload.java
+++ DashboardCore/src/test/java/com/acmerobotics/dashboard/OrbitOverload.java
@@ -22,7 +22,7 @@ public class OrbitOverload extends TestOpMode {
 
     public static double SIDE_LENGTH = 5;
 
-    public static int ORBITS = 1000;
+    public static int ORBITS = 200;
     public OrbitOverload() {
         super("Orbit Overload");
     }
